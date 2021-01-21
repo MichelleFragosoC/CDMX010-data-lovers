23bc5d1e1768f270aabd1887cc022303e0e99180
Jessi19Jassi <jessyhi44@hotmail.com>
2021-01-21 12:47:07 -0600

    Visualización de todos los personajes con fetch y el menú hamburguesa

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
 // estas funciones son de ejemplo
+import data from './data/pokemon/pokemon.js';
+
+export const pokemons = data.pokemon;
 
 export const example = () => {
   return 'example';
 };
-
+ 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//export const printData = (personajes) => {
+ // const printPersonajes = 
+//};
+
+
+export const filterByType = (type) => {
+  const filteredPokemons = pokemons.filter(function(pokemon){
+    return type === pokemon.type
+  });
+
+  return  filteredPokemons;
+}
+
+export default data;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { example } from './data.js';
+//import { pokemons } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 //import data from './data/rickandmorty/rickandmorty.js';
@@ -6,25 +6,101 @@ import { example } from './data.js';
 document.getElementById('next').addEventListener("click", function(){
     displayOne.style.display='none'; //ocultar
     displayTwo.style.display='block'; //mostrar
-})
+});
 
-const url = 'https://rickandmortyapi.com/api/character';
+const url = 'data/rickandmorty/rickandmorty.json';
 const result = document.getElementById('area');
 
+
     document.getElementById("next").addEventListener("click", function(){
+        /*let result = document.getElementById('area').innerHTML = ;*/
+        /*console.log(filteredPokemons);*/
+
+        let boton = document.getElementById('hamburguer');
+
+        function showMenu(){
+            let menu = document.getElementById('options-menu');
+
+            if(menu.classList.contains("disabled-menu")){ /*Si el elemento contiene la lista*/
+                menu.classList.remove("disabled-menu"); /*quitar clase*/
+                menu.classList.add("enabled-menu"); /*agregar clase que muestra elementos*/
+            }
+
+            else{
+                menu.classList.remove("enabled-menu"); /*quitar elementos de mostrar*/
+                menu.classList.add("disabled-menu"); /*agregar clase que oculta el menu*/
+            }
+        }
+
+        boton.addEventListener("click", showMenu);
+
         fetch(url)
         .then(response => response.json()) // Then: Promesas a cumplir
         .then(data => {
             
             let personajes = data.results;
             console.log(personajes)
-            imprimirData(personajes)
+            imprimirData(personajes) //mandando llamar la función
         });
 
-            const imprimirData = (datos) =>{  //Parametros(datos)
-                datos.forEach((element) => {
+            const imprimirData = (datos) =>{  //Parametro
+                datos.forEach((element) => { //Argumento
                     let tarjetas = `
-                    <article>
+                    <section class="card" id="cards">
+                    <div class="card">
+                    <img src="${element.image}"/> 
+                    <h2> ${element.name}</h2>
+                    <p class="status"> ${element.status}</p>
+                    <p class="gender"> ${element.gender}</p>
+                    <p> ${element.species}</p>
+                    <p> ${element.type}</p>
+                    </div>
+                    </section>
+                    `;
+                    result.insertAdjacentHTML("beforeend", tarjetas);
+                });
+            };
+
+
+    });
+
+
+//console.log(example, data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //const url = 'https://rickandmortyapi.com/api/character';
+//const result = document.getElementById('area');
+        
+        /*fetch(url)
+        .then(response => response.json()) // Then: Promesas a cumplir
+        .then(data => {
+            
+            let personajes = data.results;
+            console.log(personajes)
+            imprimirData(personajes) //mandando llamar la función
+        });
+
+            const imprimirData = (datos) =>{  //Parametro
+                datos.forEach((element) => { //Argumento
+                    let tarjetas = `
+                    <article class="card">
                     <img src="${element.image}"/> 
                     <p> ${element.name}</p>
                     <p> ${element.status}</p>
@@ -49,13 +125,12 @@ const result = document.getElementById('area');
                                 <p> ${data.species}</p>
                                 <p> ${data.type}</p>
                                 <p> ${data.location}</p>
-                                <img src="${data.image}"/>`*/
+                                <img src="${data.image}"/>`
             
-        });
+        });*/
    
 
 
 
 
 
-//console.log(example, data);
