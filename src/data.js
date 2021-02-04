b1a65091057d2744f2de3de9a8ab232309f27d0a
MichelleFragosoC <michellefragosoc@outlook.com>
2021-02-03 19:33:59 -0600

    avance

--- src/data.js
+++ src/data.js
@@ -7,3 +7,20 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//Filtrado por especie
+export const filterSpecies = (rickandmorty, value) => {
+  const array = [];
+
+  rickandmorty.forEach((object) => {
+    object.species.forEach((select_species) => {
+      if (select_species === value) {
+        array.push(object);
+      }
+    });
+  });
+  return array;
+};
+
+
+//------------------------------------------------
--- src/main.js
+++ src/main.js
@@ -1,64 +1,61 @@
 //import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
+//Importando funciones de data.js
+import { filterSpecies } from './data.js'; 
+let rickandmorty = data.results;
+console.log(rickandmorty);
+
+
 
 document.getElementById('next').addEventListener("click", function(){
     displayOne.style.display='none'; //ocultar
     displayTwo.style.display='block'; //mostrar
-})
+});
 
-const url = 'https://rickandmortyapi.com/api/character/23';
-const result = document.getElementById('area');
+const showList = personajes => {
+    const listChar = document.getElementById("area");
+    let showAll = personajes.map((element) =>{
+        return  `
+            <div class="card">
+                <img src="${element.image}"/> 
+                <div>
+                    <h4> Name: ${element.name}</h4>
+                    <p class="status"> Status: ${element.status}</p>
+                    <p class="gender"> Gender: ${element.gender}</p>
+                    <p class="origin"> Origin: ${element.origin.name}</p>
+                    <p> Species: ${element.species}</p>
+                    <p> Type: ${element.type}</p>
+                </div>
+            </div> 
+            `;
+        }).join(" ");
+        document.getElementById("area").innerHTML = showAll;
+}
 
-    document.getElementById("next").addEventListener("click", function(){
-        fetch(url)
-        .then(response => response.json()) // Then: Promesas a cumplir
-        .then(data => {
+//------------------------------------------------------------------//
+//document.getElementById('next').addEventListener("click", function(){
+//    displayOne.style.display='none'; //ocultar
+//    displayTwo.style.display='block'; //mostrar
+//})
+
+//const url = 'https://rickandmortyapi.com/api/character/23';
+//const result = document.getElementById('area');
+
+//    document.getElementById("next").addEventListener("click", function(){
+//        fetch(url)
+//        .then(response => response.json()) // Then: Promesas a cumplir
+//        .then(data => {
                   
-            result.innerHTML = `<p> ${data.name}</p>
-                                <p> ${data.status}</p>
-                                <p> ${data.gender}</p>
-                                <p> ${data.origin}</p>
-                                <p> ${data.episode}</p>
-                                <p> ${data.species}</p>
-                                <p> ${data.type}</p>
-                                <p> ${data.location}</p>
-                                <img src="${data.image}"/>`
-            console.log(data)
-        });
-   });
+//            result.innerHTML = `<p> ${data.name}</p>
+//                                <p> ${data.status}</p>
+//                                <p> ${data.gender}</p>
+//                              <p> ${data.origin}</p>
+//                                <p> ${data.episode}</p>
+//                                <p> ${data.species}</p>
+ //                               <p> ${data.type}</p>
+//                                <p> ${data.location}</p>
+//                                <img src="${data.image}"/>`
+//            console.log(data)
+//        });
+//   });
 
-
-
-   //INTENTOS MICH-------------------------------------
-// import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-/*import data from './data/rickandmorty/rickandmorty.js';
-//Pantalla de INICIO
-document.getElementById('displayOne').style.display='block';
-document.getElementById('displayTwo').style.display='none';
-
-//BOTÓN COMENZAR
-document.getElementById('start').addEventListener('click', () =>{
-    document.getElementById('displayOne').style.display='none';
-    document.getElementById('displayTwo').style.display='block';
-  });
-
-//Fetch del json que contiene la data de los pokemones
-/*const jsonFile = './data/rickandmorty/rickandmorty.js';
-let jsonRickMorty = "";
-fetch(jsonFile)
-  .then(response => response.json())
-  .then(jsonData => {
-    initialWindow(jsonData);
-    return jsonPokemon = jsonData;
-  })*/
-
-//Imprime tarjetas de todos los pokemones al cargar la pagina
-/*const initialWindow = (jsonRickMorty) => {
-
-}*/
-
-/*console.log(example, data);*/
