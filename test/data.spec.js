3e2dbb67a73d8525835660c135ecce0f8ed6fec7
MichelleFragosoC <michellefragosoc@outlook.com>
2021-02-09 21:28:52 -0600

    Testing

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,34 +1,64 @@
-import { example, anotherExample, filterByType } from '../src/data.js';
+import { filterByGender, filterByStatus, orderData} from '../src/data.js';
+import data from '../src/data/rickandmorty/rickandmorty.js';
 
+let personajes = data.results;
 
-describe('example', () => {
+describe('filterByGender', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterByGender).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns female characters when selecting filtering by female gender', () => {
+    const filterByFemale = filterByGender(personajes, 'Female')
+    expect(filterByFemale.length).toBe(73);
+  });
+
+  it('returns female charecters when selecting filtering by female gender', () => {
+    const filterByMale = filterByGender(personajes, 'Male')
+    expect(filterByMale.length).toBe(372);
+  });
+
+  it('returns female charecters when selecting filtering by female gender', () => {
+    const filterByUnkwonG = filterByGender(personajes, 'unknown')
+    expect(filterByUnkwonG.length).toBe(42);
+  });
+
+});
+
+describe('filterByStatus', () => {
+  it('is a function', () => {
+    expect(typeof filterByStatus).toBe('function');
+  });
+
+  it('returns alive characters when selecting filtering by alive status', () => {
+    const filterByAlive = filterByStatus(personajes, 'Alive')
+    expect(filterByAlive.length).toBe(281);
+  });
+
+  it('returns dead characters when selecting filtering by dead status', () => {
+    const filterByDead = filterByStatus(personajes, 'Dead')
+    expect(filterByDead.length).toBe(147);
+  });
+ 
+  it('returns unknown characters when selecting filtering by unknown status', () => {
+    const filterByUnknownS = filterByStatus(personajes, 'unknown')
+    expect(filterByUnknownS.length).toBe(65);
   });
 });
 
 
-describe('anotherExample', () => {
+describe('orderData', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof orderData).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
-
-describe('filterByType', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+  it('returns the characters sorted alphabetically A-Z', () => {
+    const sortAZ = orderData(personajes, 'name')
+    expect(sortAZ.length).toBe('493');
   });
 
-  it('returns poison pokemons when the type passed is poison', () => {
-    const pokemons = filterByType('poison')
-    expect(pokemons.length).toBe(30);
+  it('returns the characters sorted alphabetically Z-A', () => {
+    const sortZA = orderData(personajes, 'name')
+    expect(sortZA.length).toBe('493');
   });
 });
