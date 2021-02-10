9267dec71f02b00213a8b5df721959f5776152c7
MichelleFragosoC <michellefragosoc@outlook.com>
2021-02-09 20:43:11 -0600

    Testing

--- test/data.spec.js
+++ test/data.spec.js
@@ -22,4 +22,69 @@ describe('anotherExample', () => {
   });
 });
 
-/*---------------------------------------------------------------
+//---------------------------------------------------------------
+
+import { filterByGender, filterByStatus, orderData} from '../src/data.js';
+import rickandmorty from '../src/data/rickandmorty/rickandmorty.js';
+
+let personajes = rickandmorty.results;
+
+describe('filterByGender', () => {
+  it('is a function', () => {
+    expect(typeof filterByGender).toBe('function');
+  });
+
+  it('returns female characters when selecting filtering by female gender', () => {
+    const filterByFemale = filterByGender(personajes, 'Female')
+    expect(filterByFemale.length).toBe(73);
+  });
+
+  it('returns female charecters when selecting filtering by female gender', () => {
+    const filterByMale = filterByGender(personajes, 'Male')
+    expect(filterByMale.length).toBe(372);
+  });
+
+  it('returns female charecters when selecting filtering by female gender', () => {
+    const filterByUnkwonG = filterByGender(personajes, 'unknown')
+    expect(filterByUnkwonG.length).toBe(42);
+  });
+
+});
+
+describe('filterByStatus', () => {
+  it('is a function', () => {
+    expect(typeof filterByStatus).toBe('function');
+  });
+
+  it('returns alive characters when selecting filtering by alive status', () => {
+    const filterByAlive = filterByStatus(personajes, 'Alive')
+    expect(filterByAlive.length).toBe(281);
+  });
+
+  it('returns dead characters when selecting filtering by dead status', () => {
+    const filterByDead = filterByStatus(personajes, 'Dead')
+    expect(filterByDead.length).toBe(147);
+  });
+ 
+  it('returns unknown characters when selecting filtering by unknown status', () => {
+    const filterByUnknownS = filterByStatus(personajes, 'unknown')
+    expect(filterByUnknownS.length).toBe(65);
+  });
+});
+
+
+describe('orderData', () => {
+  it('is a function', () => {
+    expect(typeof orderData).toBe('function');
+  });
+
+  it('returns the characters sorted alphabetically A-Z', () => {
+    const sortAZ = orderData(personajes, 'name')
+    expect(sortAZ.length).toBe('493');
+  });
+
+  it('returns the characters sorted alphabetically Z-A', () => {
+    const sortZA = orderData(personajes, 'name')
+    expect(sortZA.length).toBe('493');
+  });
+});
